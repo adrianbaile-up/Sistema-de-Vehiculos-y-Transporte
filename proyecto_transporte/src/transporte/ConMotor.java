bc9f3f5f46394594a2636fb9e89d72dd7267b81a
adrianbaile-up <adrian.baile@bootcamp-upgrade.com>
2025-03-02 19:05:23 +0100

    Creación Interfaces
    Creación interfaces `ConMotor` y `SinMotor`.

---
+++ proyecto_transporte/src/transporte/ConMotor.java
@@ -0,0 +1,11 @@
+package transporte;
+
+public interface ConMotor {
+
+    /**
+     * Método declarado pero no definido para encenderMotor
+     */
+
+    void encenderMotor();
+
+}
---
+++ proyecto_transporte/src/transporte/SinMotor.java
@@ -0,0 +1,11 @@
+package transporte;
+
+public interface SinMotor {
+
+     /**
+     * Método declarado pero no definido para usarFuerzaHumana
+     */
+
+    void usarFuerzaHumana();
+
+}
